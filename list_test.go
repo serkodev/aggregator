aa500b0e9cee4742c0ccbed7bdac6069239bb095
SerKo <serko.dev@gmail.com>
2022-02-28 16:02:35 +0800

    update list test cases

--- list_test.go
+++ list_test.go
@@ -1,6 +1,7 @@
 package aggregator
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -8,6 +9,7 @@ import (
 func TestList(t *testing.T) {
 	aggrList := NewList(
 		New(func(k []string) (map[string]string, error) {
+			fmt.Println("list0", k)
 			return map[string]string{
 				"key1": "val1",
 				"key2": "val2",
@@ -15,36 +17,40 @@ func TestList(t *testing.T) {
 			}, nil
 		}, 10*time.Millisecond, 10),
 		New(func(k []string) (map[string]string, error) {
+			fmt.Println("list1", k)
 			return map[string]string{
 				"key1": "val1",
 				"key2": "val2",
 				"key3": "val3",
 				"key4": "val4",
 			}, nil
-		}, 100*time.Millisecond, 10),
+		}, 50*time.Millisecond, 10),
 		New(func(k []string) (map[string]string, error) {
+			fmt.Println("list2", k)
 			return map[string]string{
 				"key4": "val4",
 				"key5": "val5",
 			}, nil
-		}, 100*time.Millisecond, 10),
+		}, 50*time.Millisecond, 10),
 	).Run()
 
-	assertEqual(t, aggrList.QueryValue("key1"), "val1")
-	assertEqual(t, aggrList.QueryValue("key2"), "val2")
-	assertEqual(t, aggrList.QueryValue("key3"), "val3")
-	assertEqual(t, aggrList.QueryValue("key4"), "val4")
-	assertEqual(t, aggrList.QueryValue("key5"), "val5")
-	assertEqual(t, aggrList.Query("key6").Error == NoResult, true)
+	t.Run("Query", func(t *testing.T) {
+		t.Parallel()
+		assertEqual(t, aggrList.QueryValue("key1"), "val1")
+		assertEqual(t, aggrList.QueryValue("key4"), "val4")
+		assertEqual(t, aggrList.QueryValue("key5"), "val5")
+	})
 
-	// QueryMulti
-	results := aggrList.QueryMulti([]string{"key1", "key2"})
-	for i, result := range results { // avoid using reflect.DeepEqual with errors
-		switch i {
-		case 0:
-			assertEqual(t, result.Value, "val1")
-		case 1:
-			assertEqual(t, result.Value, "val2")
+	t.Run("Query error", func(t *testing.T) {
+		t.Parallel()
+		assertEqual(t, aggrList.Query("key6").Error == NoResult, true)
+	})
+
+	t.Run("QueryMulti", func(t *testing.T) {
+		t.Parallel()
+		results := aggrList.QueryMulti([]string{"key1", "key2", "key3", "key4", "key5"})
+		for i, result := range results { // avoid using reflect.DeepEqual with errors
+			assertEqual(t, result.Value, fmt.Sprintf("val%d", i+1))
 		}
-	}
+	})
 }
